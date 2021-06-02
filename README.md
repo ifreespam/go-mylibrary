a0844817ea4f8a89d857994147e320091dee81c0
ifreespam <ifreespam@gmail.com>
2021-06-02 15:38:30 +0300

    some fixes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # go-mylibrary
+
+We're trying to do something useful here :)
+
+Let's see what we can do with it...
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,14 +1,16 @@
 package logger
 
+import "fmt"
+
 // CustomLogger is a custom logger instance
 type CustomLogger struct {
 	label string
 }
 
 // Log function prints the string data
-func (cl *CustomLogger) Log(str string) string {
+func (cl *CustomLogger) Log(str string) {
 	var logString = "[" + cl.label + "]: " + str
-	return logString
+	fmt.Println(logString)
 }
 
 // GetLogger is a factory to create the logger instance
