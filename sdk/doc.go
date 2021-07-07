34c33e7a603f59e4533c3664686cc6e33152be25
ifreespam <ifreespam@gmail.com>
2021-07-07 15:03:42 +0300

    added go documentation

---
+++ sdk/doc.go
@@ -0,0 +1,24 @@
+// Copyright 2021
+//
+// Use of this source code is governed by an Apache License that can be found in the LICENSE file.
+
+/*
+Package sdk provides a client for using API
+Official API doc: https://google.com
+
+Usage:
+
+	import (
+		"github.com/some/sdk/v2"
+	)
+
+	client := NewClient(&ClientOptions{
+		UserName:   "UserName",
+		ApiUser:    "ApiUser",
+		ApiKey:     "ApiKey",
+		ClientIp:   "10.10.10.10",
+	})
+
+You're welcome to post issues and send your pull requests.
+*/
+package sdk
